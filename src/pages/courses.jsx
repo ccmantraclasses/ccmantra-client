90505a9efe8fead2db611a972f8a7f976219d830
ccmantraclasses <150410776+ccmantraclasses@users.noreply.github.com>
2023-12-07 19:24:42 +0530

    Update courses.jsx

--- src/pages/courses.jsx
+++ src/pages/courses.jsx
@@ -117,7 +117,7 @@ const CourseIndex = () => {
       <Box
         sx={{
           width: '100%',
-          height: 50,
+          height: 10,
           bgcolor: '#E9204F',
           display: 'flex',
           justifyContent: 'flex-end',
