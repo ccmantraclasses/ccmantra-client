a0290206f90246a113ec11b051b9cb38eef848a9
ccmantraclasses <150410776+ccmantraclasses@users.noreply.github.com>
2023-12-07 19:18:20 +0530

    Update courses.jsx

--- src/pages/courses.jsx
+++ src/pages/courses.jsx
@@ -21,7 +21,7 @@ const CourseCard = ({ title, image, price, link }) => (
   <Card
     sx={{
       width: '100%',
-      height: 410,
+      height: 350,
       p: 3,
       display: 'flex',
       flexDirection: 'column',
